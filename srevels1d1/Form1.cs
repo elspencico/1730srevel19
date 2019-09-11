04f46ffa6c037015aaa3bff1d841e803d1af90fd
elspencico <43080321+elspencico@users.noreply.github.com>
2019-09-11 02:18:02 -0500

    Completed exercise 1D

---
+++ srevels1d1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace srevels1d1
+{
+    public partial class fmCurrencyExchange : Form
+    {
+        public fmCurrencyExchange()
+        {
+            InitializeComponent();
+        }
+
+        private void Label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LblAmount_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
